410788225211a0cebeeb8ae7e36ffa2518e7ca43
mcalsing <marcelocalsing@gmail.com>
2024-07-16 15:18:47 -0300

    feat: Implementado métodos GET e DELETE para User

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -3,6 +3,12 @@ const bcrypt = require('bcrypt');
 
 module.exports = {
 
+  async getAll(_req, res) {
+    const data = await User.find();
+
+    return res.json(data);
+  },
+
   async create(req, res) {
     const { firstName, lastName, email, password } = req.body;
     const salt = await bcrypt.genSalt(10);
@@ -11,9 +17,20 @@ module.exports = {
     const userCreated = await User.create({ firstName, lastName, email, password: encryptedPass });
 
     return res.json(userCreated);
+  },
+
+  async delete(req, res) {
+    const { id } = req.params;
+    if (id.length !== 24) {
+      return res.status(401).json({ error: "ID deve ter 24 characters"})
+    }
+
+    const deleteUser = await User.findByIdAndDelete(id);
+
+    if (deleteUser) {
+      return res.json(deleteUser);
+    }
+
+    return res.status(401).json({ error: "Registro não encontrado!" });
   }
-
-
-
-
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -9,6 +9,8 @@ routes.post('/api/projects', ProjectController.create);
 routes.put('/api/projects/:id', ProjectController.updade);
 routes.delete('/api/projects/:id', ProjectController.delete);
 
+routes.get('/user', UserController.getAll);
 routes.post('/user', UserController.create);
+routes.delete('/user/:id', UserController.delete);
 
 module.exports = routes;
