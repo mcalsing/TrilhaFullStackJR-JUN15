0cf821565c13919c423e154036fb09358ac77d6e
mcalsing <marcelocalsing@gmail.com>
2024-07-16 14:43:52 -0300

    feat: Implmentado criptografia da senha do usuário antes de salvar no banco de dados

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -1,11 +1,14 @@
 const User = require('../models/UserModel');
+const bcrypt = require('bcrypt');
 
 module.exports = {
 
   async create(req, res) {
     const { firstName, lastName, email, password } = req.body;
-    
-    const userCreated = await User.create({ firstName, lastName, email, password });
+    const salt = await bcrypt.genSalt(10);
+    const encryptedPass = await bcrypt.hash(password, salt);
+
+    const userCreated = await User.create({ firstName, lastName, email, password: encryptedPass });
 
     return res.json(userCreated);
   }
