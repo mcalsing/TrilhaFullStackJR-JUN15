7821ae8c3bbd282ab521ae85ee59516a9dc9ad1b
mcalsing <marcelocalsing@gmail.com>
2024-07-19 18:05:31 -0300

    feat: Footer e ajustes de responsividade

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -18,9 +18,11 @@ export default function Home() {
   return (
     <main className="flex flex-col">
       <div className="h-[500px] lg:mx-16 xl:mx-28 2xl:mx-48 grid grid-cols-2">
-        <div className="bg-[#ef4444]  pl-10 rounded-s-md">
+        <div className="bg-[#ef4444] rounded-md relative">
           {/* <Image className="relative invert" src="/black_ink_circle.png" alt="logo" width={500} height={24}></Image> */}
-          <Image className="absolute top-32 ml-32" src="/menina_fundo.png" alt="log" width={300} height={24}></Image>
+          <Image className="absolute top-14 ml-32 " src="/menina_fundo.png" alt="log" width={300} height={24}></Image>
+          <h1 className="absolute xl:right-8 2xl:right-14 xl:top-6 2xl:top-10 top-1 text-black lg:text-lg xl:text-2xl 2xl:text-3xl font-bold">Código Certo</h1>
+          <h1 className="absolute xl:right-0 2xl:right-6 xl:top-14 2xl:top-20 top-7 text-white lg:text-md xl:text-lg 2xl:text-xl">Construindo o amanhã, hoje.</h1>
         </div>
         <div className="bg-black text-xl flex flex-col items-center justify-center rounded-s-md">
           <h1 className="text-3xl text-white">Bem-Vindo!</h1>
@@ -57,7 +59,9 @@ export default function Home() {
           </form>
         </div>
       </div>
-      <div>footer</div>
+      <footer className="absolute bottom-0 bg-red-500 text-white w-full py-2">
+        <span class="block text-sm text-white sm:text-center">© 2024 <a href="https://www.linkedin.com/in/marcelocalsing/" target="_blank">Marcelo Calsing</a>. All Rights Reserved.</span>
+      </footer>
     </main>
   );
 }
