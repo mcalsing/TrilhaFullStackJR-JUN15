8f527ad700027c0bf2aa578bfbebb4ba1dc31a57
mcalsing <marcelocalsing@gmail.com>
2024-09-05 10:09:16 -0300

    feat: Alterado endereço do CORS para o Render

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ conn();
 
 const app = express();
 app.use(express.json());
-app.use(cors({origin: 'http://localhost:3000'}));
+app.use(cors({origin: 'https://trilhafullstack.onrender.com'}));
 app.use(routes);
 
 app.listen(3334, function() {
